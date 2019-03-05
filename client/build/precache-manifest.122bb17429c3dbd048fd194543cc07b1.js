5fb62fe843ae109fef58cf4b91a4eb46c5f307c6
Smushed <smushedcode@gmail.com>
2019-03-04 19:45:58 -0600

    Creating build file off dev branch

---
+++ client/build/precache-manifest.122bb17429c3dbd048fd194543cc07b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "91f43df8dee71511a2449c21168c37f9",
+    "url": "/static/media/wormlong2.91f43df8.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "84d4fbacbcb023ec6280",
+    "url": "/static/js/main.84d4fbac.chunk.js"
+  },
+  {
+    "revision": "76c21f169973e09b2ee8",
+    "url": "/static/js/2.76c21f16.chunk.js"
+  },
+  {
+    "revision": "84d4fbacbcb023ec6280",
+    "url": "/static/css/main.052c4e7b.chunk.css"
+  },
+  {
+    "revision": "76c21f169973e09b2ee8",
+    "url": "/static/css/2.b854b72e.chunk.css"
+  },
+  {
+    "revision": "78fdab502f950153f3d2ce186dca649e",
+    "url": "/index.html"
+  }
+];
